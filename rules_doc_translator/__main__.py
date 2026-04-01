3c117c9bed2a11c3a07d377e498408ed472c0df7
Eric03742 <eric03742@foxmail.com>
2026-04-02 03:41:41 +0800

    💥 添加翻译工具将规则文本在YAML与JSON格式之间相互转换.

--- rules_doc_translator/__main__.py
+++ rules_doc_translator/__main__.py
@@ -1,11 +1,19 @@
 from argparse import ArgumentParser
 
 from rules_doc_translator.localize_NRDB import localize_NRDB
+from rules_doc_translator.to_Paratranz import yaml2json
+from rules_doc_translator.from_Paratranz import json2yaml
 
 
 parser = ArgumentParser()
 parser.add_argument("--nrdb", const=True, help="Generate NRDB card info", action="store_const")
+parser.add_argument("--yaml2json", const=True, help="Generate texts in JSON", action="store_const")
+parser.add_argument("--json2yaml", const=True, help="Generate texts in YAML", action="store_const")
 args = parser.parse_args()
 
 if args.nrdb is not None:
     localize_NRDB()
+elif args.yaml2json is not None:
+    yaml2json()
+elif args.json2yaml is not None:
+    json2yaml()
